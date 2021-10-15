433ed4caab79a6a4d7bb5094947b070e7605d86a
Pandemic1617 <43860275+Pandemic1617@users.noreply.github.com>
2021-10-15 14:00:19 +0200

    removed public api key
    This is a public api key and has to be included in the deployed build anyway
    The api key acts more like an id than an api key with permissions
    I still decided to disable this api key and replace it with a new api key as a precaution
    https://firebase.google.com/docs/projects/api-keys

--- client/src/js/analytics.js
+++ client/src/js/analytics.js
@@ -1,15 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/analytics";
-
-var firebaseConfig = {
-    apiKey: "AIzaSyD3-hPEKEf99rGyBy84z7GR-pGrErluJpU",
-    authDomain: "gucschedule.firebaseapp.com",
-    projectId: "gucschedule",
-    storageBucket: "gucschedule.appspot.com",
-    messagingSenderId: "621973797621",
-    appId: "1:621973797621:web:56ac02c13ae4653521a3bc",
-    measurementId: "G-LB7NRX352B",
-};
+import { firebaseConfig } from "./secret";
 
 const app = firebase.initializeApp(firebaseConfig);
 const analytics = app.analytics();
