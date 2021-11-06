44844e25427da361d0cd39d11593a781a44f018e
Pandemic1617 <43860275+Pandemic1617@users.noreply.github.com>
2021-11-06 20:43:22 +0200

    added appVersion to User Properties
    added appVersion to User Properties to appear in google analytics

--- client/src/js/analytics.js
+++ client/src/js/analytics.js
@@ -1,10 +1,11 @@
 import { initializeApp } from "firebase/app";
-import { logEvent as rawLogEvent, initializeAnalytics } from "firebase/analytics";
+import { logEvent as rawLogEvent, initializeAnalytics, setUserProperties } from "firebase/analytics";
 import { firebaseConfig } from "./secret";
 import { appVersion } from "./consts";
 
 const app = initializeApp(firebaseConfig);
 const analytics = initializeAnalytics(app, { config: { connection: "online", appVersion } });
+setUserProperties(analytics, { appVersion });
 const logEvent = (eventName, eventParams) => rawLogEvent(analytics, eventName, { ...eventParams });
 
 export { logEvent };
